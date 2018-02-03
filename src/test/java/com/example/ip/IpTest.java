d559ad44417f4049405d73827a9989db99bf1177
Anton_Guba <Anton_Guba@epam.com>
2018-02-03 16:17:19 +0100

    Add Ip Test

---
+++ src/test/java/com/example/ip/IpTest.java
@@ -0,0 +1,15 @@
+package com.example.ip;
+
+import static io.restassured.RestAssured.get;
+
+import org.hamcrest.CoreMatchers;
+import org.testng.annotations.Test;
+
+public class IpTest {
+  private static final String URL = "http://ip.jsontest.com/";
+  @Test
+  public void checkErrorStatusForURLs() {
+    get(URL).then().statusCode(CoreMatchers.equalTo(200));
+  }
+
+}
